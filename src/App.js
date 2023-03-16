6ca2af867a923562be2a0ea376f3c8cf1fbaa55e
Ahme Erdoğan <a.erdogan6868@gmail.com>
2023-03-16 13:52:13 +0300

    Monster Rolodex

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import { Component } from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState(() => {
+        return { monsters: users }
+      },
+        () => {
+          console.log(this.state)
+        }))
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+     
+
+    this.setState(() => {
+      return { searchField }
+    })
+  }
+
+  render() {
+
+    const { monsters, searchField} = this.state;
+    const {onSearchChange} = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField)
+    })
+
+    return (
+      <div className='app'>
+        <input className='search-box' type="search" placeholder="search monsters" onChange={onSearchChange} />
+
+        {
+          filteredMonsters.map((monster) => {
+            return <h1 key={monster.id}>{monster.name}</h1>
+          })
+        }
+      </div>
+    )
+  }
+}
+
+export default App;
